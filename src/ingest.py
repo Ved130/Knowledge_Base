f4029c0163c2af021460f747fd2cd45c8f4c74b2
Ved130 <pashine.ved@gmail.com>
2026-07-23 10:54:47 +0530

    Fix makedirs typo

--- src/ingest.py
+++ src/ingest.py
@@ -82,7 +82,7 @@ def ingest_youtube(url:str):
 
 def ingest_all_pdfs():
     pdf_folder = "./data/pdfs"
-    os.mkdirs(pdf_folder,exist_ok = True)
+    os.makedirs(pdf_folder,exist_ok = True)
     for filename in os.listdir(pdf_folder):
         if filename.endswith(".pdf"):
             ingest_pdf(os.path.join(pdf_folder,filename))
